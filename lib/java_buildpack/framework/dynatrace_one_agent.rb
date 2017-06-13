dfcc4ff97cafbe97a87b17afab6f8ed5486070ad
aripka-pivotal <aripka@gopivotal.com>
2017-06-13 17:47:34 -0500

    let's try no https

--- lib/java_buildpack/framework/dynatrace_one_agent.rb
+++ lib/java_buildpack/framework/dynatrace_one_agent.rb
@@ -115,7 +115,7 @@ module JavaBuildpack
         print api_base_url
         #download_uri = "#{api_base_url}/v1/deployment/installer/agent/unix/paas/latest?include=java&bitness=64&"
         #download_uri += "Api-Token=#{credentials[APITOKEN]}"
-        download_uri = "https://jar-host.apps.pivotalmn.com/paasInstaller_Unix_64.zip"
+        download_uri = "http://jar-host.apps.pivotalmn.com/paasInstaller_Unix_64.zip"
         ['latest', download_uri]
       end
 
